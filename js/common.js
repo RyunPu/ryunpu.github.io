d3df1cf60ea36ee5726f74eab21847bccfed4f32
Yuanmin Pu <endless3000@163.com>
2015-05-10 21:35:28 +0800

    detail tweaking

--- js/common.js
+++ js/common.js
@@ -1,14 +1,3 @@
 $(function() {
-    var currentHours = new Date().getHours(),
-        $body = $('body');
-
-    if (currentHours >= 17 && currentHours <= 19) {
-        $body.attr('class', 'sunset');
-    } else if ((currentHours > 19 && currentHours <= 24) || currentHours < 5) {
-        $body.attr('class', 'night');
-    } else if (currentHours >= 5 && currentHours <= 7) {
-        $body.attr('class', 'dawn');
-    } else {
-        $body.attr('class', 'daylight');
-    }
+    
 });
