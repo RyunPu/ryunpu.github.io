cc16c24fee29187667d3d6039f3ebd3ceb6a880b
Yuanmin Pu <endless3000@163.com>
2015-06-09 12:47:42 +0800

    choose your favorite time of the day

--- js/common.js
+++ js/common.js
@@ -1,3 +1,37 @@
 $(function() {
-    
+    var Page = {
+        init: function() {
+            this.bind();
+        },
+        bind: function() {
+            $('.footer .choose a').on('click', function() {
+                var time = $(this).html();
+
+                switch (time) {
+                    case 'dawn':
+                        $body.attr('class', 'dawn');
+                        localStorage.time = 'dawn';
+                        break;
+                    case 'daylight':
+                        $body.attr('class', 'daylight');
+                        localStorage.time = 'daylight';
+                        break;
+                    case 'sunset':
+                        $body.attr('class', 'sunset');
+                        localStorage.time = 'sunset';
+                        break;
+                    case 'night':
+                        $body.attr('class', 'night');
+                        localStorage.time = 'night';
+                        break;
+                    case 'all':
+                        localStorage.time = '';
+                        location.reload();
+                        break;
+                }
+            });
+        }
+    };
+
+    Page.init();
 });
